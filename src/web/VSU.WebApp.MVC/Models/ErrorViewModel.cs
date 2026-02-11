45392f4dfa580bfbbcafb59a9d3af1ee8dbb0eaa
Fabio S Medeiros <fabiosmedeiros@gmail.com>
2026-02-11 12:15:23 -0300

    Add project files.

---
+++ src/web/VSU.WebApp.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace VSU.WebApp.MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
