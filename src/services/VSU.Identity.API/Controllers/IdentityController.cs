c5c33ec4e2166974f899c8773d0c123aa5a39c32
Fabio S Medeiros <fabiosmedeiros@gmail.com>
2026-02-19 12:20:29 -0300

    Adicionando uma classe de contexto de banco de dados. Adicionando view models para o registro e login de usuários. Adicionando a string de conexão ao arquivo appsettings.json. Execução de migrations, com criação da base de dados. Inclusão de PackageReference Swagger no arquivo VSU.Identity.API.csproj. Alterando a classe Startup.cs, incluindo configuração do contexto do banco de dados, identity, e swagger. Alterando o launchSettings.json, para inicializar a aplicação com o swagger. Adicionando um arquivo global.json, para definir a versão do SDK (3.1.426). Removendo a classe WeatherForecast. Removendo o controller WeatherForecastController.

---
+++ src/services/VSU.Identity.API/Controllers/IdentityController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using VSU.Identity.API.Models;
+
+namespace VSU.Identity.API.Controllers
+{
+    [ApiController]
+    [Route("api/identidade")]
+    public class IdentityController : Controller
+    {
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        private readonly UserManager<IdentityUser> _userManager;
+
+
+        public IdentityController(SignInManager<IdentityUser> signInManager, UserManager<IdentityUser> userManager)
+        {
+            this._signInManager = signInManager;
+            this._userManager = userManager;
+        }
+
+        [HttpPost("criar-nova-conta")]
+        public async Task<ActionResult> Registrar(UsuarioRegistro usuarioRegistro)
+        {
+            if (!ModelState.IsValid) return BadRequest();
+
+            var user = new IdentityUser
+            {
+                UserName = usuarioRegistro.Email,
+                Email = usuarioRegistro.Email,
+                EmailConfirmed = true
+            };
+
+            var result = await _userManager.CreateAsync(user, usuarioRegistro.Senha);
+
+            if (result.Succeeded)
+            {
+                await _signInManager.SignInAsync(user, false);
+
+                return Ok();
+            }
+
+            return BadRequest();
+        }
+
+        [HttpPost("autenticar")]
+        public async Task<ActionResult> Login(UsuarioLogin usuarioLogin)
+        {
+            if (!ModelState.IsValid) return BadRequest();
+
+            var result = 
+                await _signInManager.PasswordSignInAsync(usuarioLogin.Email, usuarioLogin.Senha, false, true);
+
+            if (result.Succeeded)
+            {
+                return Ok();
+            }
+
+            return BadRequest();
+        }
+    }
+}
--- src/services/VSU.Identity.API/Startup.cs
+++ src/services/VSU.Identity.API/Startup.cs
@@ -1,15 +1,13 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using VSU.Identity.API.Data;
 
 namespace VSU.Identity.API
 {
@@ -25,12 +23,35 @@ namespace VSU.Identity.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddDefaultIdentity<IdentityUser>()
+                .AddRoles<IdentityRole>()
+                .AddEntityFrameworkStores<AppDbContext>()
+                .AddDefaultTokenProviders();
             services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo 
+                { 
+                    Title = "Virtua Shop Uno",
+                    Description = "App Virtua Shop Uno - Projeto exemplo",
+                    Contact = new OpenApiContact() { Name = "Fabio S Medeiros", Email = "fabiosmedeiros@gmail.com" },
+                    License = new OpenApiLicense() { Name = "MIT", Url = new Uri("https://opensource.org/licenses/MIT") }
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -39,6 +60,8 @@ namespace VSU.Identity.API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseAuthentication();
 
             app.UseAuthorization();
 
