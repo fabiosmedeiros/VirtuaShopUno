19b9554622e07c3ae83ccfca6438dde09a6ceb0f
Fabio S Medeiros <fabiosmedeiros@gmail.com>
2026-02-19 22:26:38 -0300

    Alteração do controller IdentityController, com inclusão de um método para geração de Json Web Token. Criação do método ToUnixEpochDate, para representação do timestamp.. Criação da classe AppSettings, para mapeamento das configurações definidas no appsettings.Development.json. Inclusão das models UsuarioClaim, UsuarioToken e UsuarioRespostaLogin no arquivo UserViewModel.cs. Definição de configurações no appsettings.Development.json.

---
+++ src/services/VSU.Identity.API/Extensions/AppSettings.cs
@@ -0,0 +1,13 @@
+﻿namespace VSU.Identity.API.Extensions
+{
+    public class AppSettings
+    {
+        public string Secret { get; set; }
+
+        public double ExpirationTime { get; set; }
+
+        public string Emissor { get; set; }
+
+        public string ValidAt { get; set; }
+    }
+}
